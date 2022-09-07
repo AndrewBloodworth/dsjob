a62792deab4d0e9ff04e8b20bd88c63bd856cc48
Andrew Bloodworth <andrew.bloodworth@bluecore.com>
2022-09-06 22:40:12 -0600

    l

---
+++ f2.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+module.exports = async (file_name, name_space) => {
+  try {
+    require("dotenv").config();
+    const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+    const { data } = await axios.get(
+      `https://api.github.com/repos/TriggerMail/${name_space}/contents/${file_name}?ref=master`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+          Accept: "application/vnd.github.v4.raw",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    return "";
+  }
+};
